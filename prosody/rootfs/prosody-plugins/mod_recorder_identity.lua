c5840566c234b6b888599bcb419014b470ea7191
tu-nv <vantu.bkhn@gmail.com>
2020-07-06 00:39:43 +0000

    prosody plugin to add recorder info to file sharing metadata the current purpose is for jibri to get the recoder user information, so that jibri can send link to download recorded files to the recorder user

---
+++ prosody/rootfs/prosody-plugins/mod_recorder_identity.lua
@@ -0,0 +1,22 @@
+local st = require "util.stanza";
+local json = require "cjson";
+
+-- detect new jibri start recording action and add recorder_identity info to metadata
+module:hook("pre-iq/full", function(event)
+    local stanza = event.stanza;
+    if stanza.name == "iq" then
+        local jibri = stanza:get_child('jibri', 'http://jitsi.org/protocol/jibri');
+        if jibri and jibri.attr.action == 'start' then
+            local recorder_identity = event.origin.jitsi_meet_context_user;
+            if recorder_identity then
+                log('info', "new recording session by: " .. recorder_identity.email);
+                -- inject recorder_identity info field to file recording metadata
+                local app_data = json.decode(jibri.attr.app_data);
+                app_data.file_recording_metadata.recorder_identity = recorder_identity;
+                jibri.attr.app_data = json.encode(app_data);
+            else
+                log('warning', "new recording without recorder_identity info");
+            end
+        end
+    end
+end);
