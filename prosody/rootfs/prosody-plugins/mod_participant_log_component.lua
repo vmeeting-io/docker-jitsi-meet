76c0967a59a681c6e306b95e191767ae803b07c9
SangwooRyu <mrryu006@naver.com>
2020-11-27 14:52:59 +0900

    revise participant_log prosody plugin

--- prosody/rootfs/prosody-plugins/mod_participant_log_component.lua
+++ prosody/rootfs/prosody-plugins/mod_participant_log_component.lua
@@ -3,8 +3,10 @@ local socket = require "socket";
 local json = require "util.json";
 local ext_events = module:require "ext_events";
 local it = require "util.iterators";
+local jid = require "jtil.jid";
 local jid_resource = require "util.jid".resource;
 local is_healthcheck_room = module:require "util".is_healthcheck_room;
+local http = require "net.http";
 
 -- we use async to detect Prosody 0.10 and earlier
 local have_async = pcall(require, "util.async");
@@ -31,28 +33,42 @@ function occupant_joined(event)
 
 
     if room.participant then
-        local users_json = {};
+        local node, host, resource = jid.split(room.jid);
 
-        room.participant[nick] = {joinTime = os.date("*t"), leaveTime = nil, sessions = occupant.sessions[occupant.jid]};
+        local url = "http://vmapi:5000/plog/participant-join";
+
+        local body = {};
+        body.room = node;
+        body.stats = {};
+
+        room.participant[nick] = {
+            joinTime = os.date("*t"),
+            leaveTime = nil,
+            name = occupant.sessions[occupant.jid]:get_child_text('nick', 'http://jabber.org/protocol/nick'),
+            email = occupant.sessions[occupant.jid]:get_child_text('email'),
+            id = occupant.sessions[occupant.jid]:get_child_text('id'),
+            jid = occupant.jid
+        };
 
         for k, v in pairs(room.participant) do
-            users_json[k] = v;
+            local stat = {};
+        
+            stat.joinTime = os.time(v.joinTime);
+            stat.leaveTime = os.time(v.leaveTime);
+            stat.name = v.name);
+            stat.email = v.email;
+            stat.id = id;
+            stat.jid = jid;
+            body.stats[k] = stat;
         end
 
-        local body_json = {};
-        body_json.type = 'participant_log';
-        body_json.log = users_json;
+        local encoded_body = json.encode(body);
 
-        for k, v in pairs(room._occupants) do
-            local stanza = st.message({
-                from = module.host;
-                to = v.jid;
-            })
-            :tag("json-message", {xmlns='http://jitsi.org/jitmeet'})
-            :text(json.encode(body_json)):up();
-
-            room:route_stanza(stanza);
-        end
+        -- https://prosody.im/doc/developers/net/http
+        http.request(url, { body=encoded_body, method="POST", headers = { ["Content-Type"] = "application/json" } },
+        function(resp_body, response_code, response)
+            print(resp_body, response_code, response);
+        end);
     end
 end
 
@@ -69,34 +85,84 @@ function occupant_leaving(event)
     local logForOccupant = room.participant[nick];
 
     if logForOccupant then
-        local users_json = {};
+        local node, host, resource = jid.split(room.jid);
 
-        room.participant[nick] = {joinTime = room.participant[nick].joinTime, leaveTime = os.date("*t"), sessions = room.participant[nick].sessions};
+        local url = "http://vmapi:5000/plog/participant-leave";
+
+        local body = {};
+        body.room = node;
+        body.stats = {};
+
+        room.participant[nick] = {
+            joinTime = room.participant[nick].joinTime,
+            leaveTime = os.date("*t"),
+            name = room.participant[nick].name,
+            email = room.participant[nick].email,
+            id = room.participant[nick].id,
+            jid = room.participant[nick].jid
+        };
 
         for k, v in pairs(room.participant) do
-            users_json[k] = v;
+            local stat = {};
+        
+            stat.joinTime = os.time(v.joinTime);
+            stat.leaveTime = os.time(v.leaveTime);
+            stat.name = v.name);
+            stat.email = v.email;
+            stat.id = id;
+            stat.jid = jid;
+            body.stats[k] = stat;
         end
 
-        local body_json = {};
-        body_json.type = 'participant_log';
-        body_json.log = users_json;
+        local encoded_body = json.encode(body);
 
-        for k, v in pairs(room._occupants) do
-            local stanza = st.message({
-                from = module.host;
-                to = v.jid;
-            })
-            :tag("json-message", {xmlns='http://jitsi.org/jitmeet'})
-            :text(json.encode(body_json)):up();
-
-            room:route_stanza(stanza);
-        end
+        -- https://prosody.im/doc/developers/net/http
+        http.request(url, { body=encoded_body, method="POST", headers = { ["Content-Type"] = "application/json" } },
+        function(resp_body, response_code, response)
+            print(resp_body, response_code, response);
+        end);
     end
 end
 
 function room_created(event)
     local room = event.room;
     room.participant = {};
+end
+
+function room_destroyed(event)
+    local room = event.room;
+
+    if is_healthcheck_room(room.jid) then
+        return;
+    end
+
+    local node, host, resource = jid.split(room.jid);
+
+    local url = "http://vmapi:5000/plog/room-destoryed";
+
+    local body = {};
+    body.room = node;
+    body.stats = {};
+
+    for k, v in pairs(room.participant) do
+        local stat = {};
+    
+        stat.joinTime = os.time(v.joinTime);
+        stat.leaveTime = os.time(v.leaveTime);
+        stat.name = v.name);
+        stat.email = v.email;
+        stat.id = id;
+        stat.jid = jid;
+        body.stats[k] = stat;
+    end
+
+    local encoded_body = json.encode(body);
+
+    -- https://prosody.im/doc/developers/net/http
+    http.request(url, { body=encoded_body, method="POST", headers = { ["Content-Type"] = "application/json" } },
+    function(resp_body, response_code, response)
+        print(resp_body, response_code, response);
+    end);
 end
 
 -- executed on every host added internally in prosody, including components
@@ -106,6 +172,20 @@ function process_host(host)
 
        local muc_module = module:context(host)
        muc_module:hook("muc-room-created", room_created, -1);
+       muc_module:hook("muc-room-destroyed", room_destroyed, -1);
+       muc_module:hook("muc-occupant-joined", occupant_joined, -1);
+       muc_module:hook("muc-occupant-pre-leave", occupant_leaving, -1);
+    end
+end
+
+-- executed on every host added internally in prosody, including components
+function process_host(host)
+    if host == muc_component_host then -- the conference muc component
+        module:log("info", "Hook to muc events on %s", host);
+
+       local muc_module = module:context(host)
+       muc_module:hook("muc-room-created", room_created, -1);
+       muc_module:hook("muc-room-destroyed", room_destroyed, -1);
        muc_module:hook("muc-occupant-joined", occupant_joined, -1);
        muc_module:hook("muc-occupant-pre-leave", occupant_leaving, -1);
     end
