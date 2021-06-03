7da0798eeded8c0a67a70470624a96a7cea41011
tu-nv <vantu.bkhn@gmail.com>
2021-06-03 06:09:53 +0000

    improve individual recordings put videos of a same conf into a folder named confName_confID multi stage convert: x264 mp4 -> vp8 mkv -> error report ensure participants always send at least thumnail video to jvb. This also improve selective on viewport forwarding

--- web/rootfs/defaults/custom-config.js
+++ web/rootfs/defaults/custom-config.js
@@ -1,6 +1,9 @@
 {{ if (.Env.ENABLE_INDIVIDUAL_REC | default "false" | toBool) }}
-config.sendMinHeight = {{ .Env.REC_RESOLUTION | default 180 }}
-
-if (!config.hasOwnProperty('p2p')) config.p2p = {};
 config.p2p.enabled = false;
 {{ end }}
+
+// with selective on viewport forwarding, participant will stop sending video if its out of the viewport in all
+// other viewports. This delay and possibly cause unstable video transmission when the participant appear in one
+// of the viewports again. We set to always send minSendFrameHeight to fix that. minSendFrameHeight also needed
+// for Individual recording to work
+config.minSendFrameHeight = Math.max({{ .Env.REC_RESOLUTION | default 0 }}, config.constraints.video.height.min)
