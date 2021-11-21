938b18d4f8f4b4436b83dc85a065af4ef8ba85c0
TaeHwan Eun <theun0524@gmail.com>
2021-11-21 04:05:28 +0000

    fix: add recording finalize for vmapi

--- jibri/rootfs/usr/bin/recording_finalize.sh
+++ jibri/rootfs/usr/bin/recording_finalize.sh
@@ -60,6 +60,7 @@ REC_FOLDER=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1)
 REC_DIR=${UPLOAD_DIR}/${REC_FOLDER}
 REC_FILE_PATH=""
 REC_FILE_NAME=""
+REC_FILE_SIZE=""
 mkdir -p ${REC_DIR}
 
 for f in ${UPLOAD_DIR}/*.{mp4,pdf}; do
@@ -72,13 +73,17 @@ for f in ${UPLOAD_DIR}/*.{mp4,pdf}; do
     new_file_name="recorded_${FDATE}.${file_extension}"
     mv $f ${REC_DIR}/${new_file_name}
     REC_FILE_NAME=${REC_DIR}/${new_file_name}
+    REC_FILE_SIZE=$(stat -c%s "$f")
     LINK="${PUBLIC_URL}${RECORDING_DOWNLOAD_BASE}/${REC_FOLDER}/${new_file_name}"
     # one line for each link
     DOWNLOAD_LINKS="${DOWNLOAD_LINKS}
 ${LINK}"
 done
 
-if [[ "$USE_AMAZON_S3" -ne "" ]]; then
+# sync everything to storage
+rsync -r $REC_DIR root@storage:/recordings
+
+if [[ "$RECORDING_FINALIZE_METHOD" -eq "s3" ]]; then
     if [[ -z $S3_ACCESS_KEY_ID ]]; then
         echo 'ERROR: S3_ACCESS_KEY_ID must be set'
         exit 1
@@ -119,10 +124,17 @@ if [[ "$USE_AMAZON_S3" -ne "" ]]; then
         -d "{\"uploadVideo\": \"${relativePath}\", \"roomUrl\": \"${URL}\"}" \
         ${S3_UPLOAD_NOTIFY_URL}
 
+elif [[ "$RECORDING_FINALIZE_METHOD" -eq "vmapi" ]]; then
+    ENDPOINT="http://vmapi:5000/recording-finalize"
+    AUTH_HEADER="Authorization: Bearer $VMEETING_DB_PASS"
+    curl -v -X POST -H "Date: $DATE" -H "$AUTH_HEADER" \
+        -H "Content-Type: application/json" \
+        -d "{\"uploadVideo\": \"$REC_FILE_NAME\", \"fileSize\": $REC_FILE_SIZE, \"roomUrl\": \"$URL\"}" \
+        "$ENDPOINT"
 else
 
-# create email content
-EMAIL_MESSAGE="\
+    # create email content
+    EMAIL_MESSAGE="\
 Vmeeting을 이용해주셔서 감사합니다.
 
 \"${MEETING_NAME}\" 회의에 대한 녹화 파일은 아래 위치에서 다운로드 받을 수 있습니다:
@@ -146,7 +158,6 @@ NOTE: The recorded file(s) will be automatically DELETED from our servers after 
 This is out-going email only.
 Copyright@2020 KeduTech, Inc. ALL RIGHTS RESERVED."
 
-if [[ $USE_AMAZON_SES -eq 1 || x$USE_AMAZON_SES == xtrue ]]; then
     if [[ -z $AWS_ACCESS_KEY_ID ]]; then
         echo 'ERROR: AWS_ACCESS_KEY_ID must be set'
         exit 1
@@ -155,9 +166,6 @@ if [[ $USE_AMAZON_SES -eq 1 || x$USE_AMAZON_SES == xtrue ]]; then
         echo 'ERROR: AWS_SECRET_ACCESS_KEY must be set'
         exit 1
     fi
-
-    # sync everything to storage
-    rsync -r $REC_DIR root@storage:/recordings
 
     # delegate to vmapi
     EMAIL_SUBJECT="[Vmeeting] Download recorded file for Vmeeting \"${MEETING_NAME}\""
@@ -182,39 +190,6 @@ if [[ $USE_AMAZON_SES -eq 1 || x$USE_AMAZON_SES == xtrue ]]; then
         --data-urlencode "$ROOM_NAME" \
         --data-urlencode "$RECORD_LINK" \
         "$ENDPOINT"
-
-else
-    # send using postech smtp server
-    if [[ -z $SMTP_SERVER ]]; then
-        echo 'ERROR: SMTP_SERVER must be set'
-        exit 1
-    fi
-    # FIXME: NOT indent the string is needed here.
-    EMAIL_HEADER="\
-From: Vmeeting <${NOREPLY_MAIL}>
-To: <${RECORDER_EMAIL}>
-Subject: [Vmeeting] Download recorded file for Vmeeting \"${MEETING_NAME}\"
-"
-    echo "$EMAIL_HEADER" > ${REC_DIR}/email.txt
-    echo "$EMAIL_MESSAGE" >> ${REC_DIR}/email.txt
-
-    # RFC 5322 require CRLF line ending for email content
-    unix2dos ${REC_DIR}/email.txt
-
-    # finally sync everything to storage
-    rsync -r $REC_DIR root@storage:/recordings
-
-    #
-    # send email about download link to the recorder user
-    # email is sent via the storage container, which runs in manager node.
-    # port 25 should be opened by the ISP for the manager node
-    #
-
-    ssh root@storage "curl --url \"$SMTP_SERVER\" \
-                    --mail-from \"$NOREPLY_MAIL\" \
-                    --mail-rcpt \"$RECORDER_EMAIL\" \
-                    --upload-file /recordings/$REC_FOLDER/email.txt"
-fi
 
 fi
 
